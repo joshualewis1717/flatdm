8ebab19cebef4baf469929c18ea8f97668ce7b60
AvniDhokia <avDhokia@protonmail.com>
2026-04-09 15:15:10 +0100

    added try catch validation for db_access

--- src/app/app/reports/db_access.tsx
+++ src/app/app/reports/db_access.tsx
@@ -11,7 +11,7 @@ import { Database, UnfoldHorizontal } from "lucide-react";
 // delete all properties (and all associated listings and applications) by a given landlord id
 async function rawDeletePropertiesByLandlord({landlordId} : {landlordId : number}){
     if (landlordId == undefined){
-        console.log("error: trying to delete properties for undefined user id");
+        console.log("error occured: trying to delete properties for undefined user id");
         return;
     }
 
@@ -19,31 +19,45 @@ async function rawDeletePropertiesByLandlord({landlordId} : {landlordId : number
     await rawDeletePropertyListingByLandlord({landlordId:landlordId});
 
     // deleting properties
-    await prisma.property.deleteMany({
-        where: {landlordId: landlordId}
-    });
-    console.log("deleted properties")
+    try{
+        await prisma.property.deleteMany({
+            where: {landlordId: landlordId}
+        });
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
+
     return;
 }
 
 // delete all property applications made by a given user id
 async function rawDeletePropertyApplicationsByUser({userId} : {userId : number}){
 
-    await prisma.propertyApplication.deleteMany({
-        where: {userId: {equals: userId}}
-    });
+    try{
+        await prisma.propertyApplication.deleteMany({
+            where: {userId: {equals: userId}}
+        });
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }    
 
-    console.log("deleted property applications by user " + userId);
     return;
 }
 
 // delete all property applications for a given listing id
 async function rawDeletePropertyApplicationsByListing({listingId} : {listingId : number}){
-    await prisma.propertyApplication.deleteMany({
-        where: {listingId: listingId}
-    });
 
-    console.log("deleted property applications for listing id " + listingId);
+    try{
+        await prisma.propertyApplication.deleteMany({
+            where: {listingId: listingId}
+        }); 
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
+
     return;
 }
 
@@ -58,49 +72,67 @@ async function rawDeletePropertyListingByLandlord({landlordId} : {landlordId : n
     // first delete all associated property applications
     for (let p = 0; p< propertyListingIds.length; p++){
         await rawDeletePropertyApplicationsByListing({listingId:propertyListingIds[p]});
-        console.log("removed property applications " + propertyListingIds[p]);
     };
     
     // finally 'delete' property listings
-    await prisma.propertyListing.deleteMany({
-        where: {landlordId: landlordId}
-    });
+    try{
+        await prisma.propertyListing.deleteMany({
+            where: {landlordId: landlordId}
+        });  
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 
-    console.log("deleted property listings")
+    return;
 }
 
 async function rawDeleteReview({reviewId} : {reviewId : number}){
-    await prisma.review.updateMany({
-        where: {id: reviewId},
-        data: { isDeleted: true }
-    });
+    try{
+        await prisma.review.updateMany({
+            where: {id: reviewId},
+            data: { isDeleted: true }
+        });
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 
-    console.log("deleted review " + reviewId);
     return;
 }
 
 // delete all reviews written by this user or about this user
 async function rawDeleteReviewByUser({userId} : {userId : number}){
-    await prisma.review.updateMany({
-        where: {
-            OR: [
-                { authorId: {equals: userId} },
-                { targetUserId: {equals: userId} }
-            ]
-        },
-        data: { isDeleted: true }
-    });
+    try{
+        await prisma.review.updateMany({
+            where: {
+                OR: [
+                    { authorId: {equals: userId} },
+                    { targetUserId: {equals: userId} }
+                ]
+            },
+            data: { isDeleted: true }
+        });
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 
-    console.log("deleted reviews linked to user " + userId);
     return;
 }
 
 // delete a user - should be done last since not recursive for other tables
 async function rawDeleteUser({userId} : {userId:number}){
-    await prisma.user.update({
-        where: {id: userId},
-        data: {isDeleted: true}
-    });
+
+   try{
+        await prisma.user.update({
+            where: {id: userId},
+            data: {isDeleted: true}
+        });
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 
     return;
 }
@@ -109,23 +141,35 @@ async function rawDeleteUser({userId} : {userId:number}){
 // functions to be used externally:
 
 export const addOffence: ConfirmFunction = async ({ user, text }) => {
-    await prisma.offenceRecord.create({
-        data: {
-            reason: text,
-            userId: user.id
-        },
-    }); 
 
-    console.log("Added offence record (" + text + ") to user ");// + user['username']);
+    try{
+        await prisma.offenceRecord.create({
+            data: {
+                reason: text,
+                userId: user.id
+            },
+        }); 
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
+
     return;
 }
 
 // change the status of a report
 export async function changeReportStatus({reportId, newStatus}){
-    await prisma.report.update({
-        where: {id: reportId},
-        data: {status: newStatus}
-    });
+
+    try{
+        await prisma.report.update({
+            where: {id: reportId},
+            data: {status: newStatus}
+        });
+ 
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 
     return;
 }
@@ -133,17 +177,22 @@ export async function changeReportStatus({reportId, newStatus}){
 // assign a moderator to a report
 export async function assignModToReport({reportId, userId}){
 
-    // make sure the user is actually a moderator
-    const role = await prisma.user.findFirst({
-        where: {id: userId},
-        select: {role}
-    })
+    try{
+        // make sure the user is actually a moderator
+        const role = await prisma.user.findFirst({
+            where: {id: userId},
+            select: {role}
+        })
 
-    if (role === "CONSULTANT"){
-        await prisma.report.update({
-            where: { id: reportId },
-            data: { assignedModeratorId: userId },
-        });
+        if (role === "CONSULTANT"){      
+            await prisma.report.update({
+                where: { id: reportId },
+                data: { assignedModeratorId: userId },
+            });
+        }
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
     }
 
     return;
@@ -158,13 +207,15 @@ export async function deleteReport({report} : Report){
     const ok = window.confirm(`Delete Report ${report['reason']}? This cannot be undone.`);
     if (!ok) return;
 
-    // delete from database
-    await prisma.report.delete({
-        where: {id: report['id']}
-    });
-
-    // confirm in logs
-    console.log("deleted report with id: " + report['id']);
+    try{
+        // delete from database
+        await prisma.report.delete({
+            where: {id: report['id']}
+        });
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 
     return;
 }
@@ -175,29 +226,19 @@ export async function getReport({reportId}: any){
         return undefined;
     }
 
-    const report = await prisma.report.findFirst({
-        where: {id: reportId}
-    })
-
-    return report;
+    try{
+        const report = await prisma.report.findFirst({
+            where: {id: reportId}
+        })
+        return report;
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 }
 
 
 export async function getReportsFilteredSorted({selectedStatuses, selectedSeverities, selectedCategories, sortField, sortDirection} : FilterSearchProps){
-
-    // const statuses = [];
-
-    // // process statuses from eg [OPEN: true, UNDER_REVIEW: false, RESOLVED: true] into eg [OPEN, RESOLVED]
-    // if (selectedStatuses.OPEN){
-    //     statuses.push("OPEN")
-    // }
-    // if (selectedStatuses.UNDER_REVIEW){
-    //     statuses.push("UNDER_REVIEW")
-    // }
-    // if (selectedStatuses.RESOLVED){
-    //     statuses.push("RESOLVED")
-    // }
-
 
     const STATUSES = ["OPEN", "UNDER_REVIEW", "RESOLVED"];
     const SEVERITIES = ["LOW", "MEDIUM", "HIGH"];
@@ -214,24 +255,34 @@ export async function getReportsFilteredSorted({selectedStatuses, selectedSeveri
     //   where: {status: {in: statuses}},
     //   orderBy: {[sortField]: sortDirection}
     // });
-    const reports = await prisma.report.findMany({
-        where: {
-            AND: [
-                {status: {in: statuses}},
-                {severity: {in: severities}},
-                {category: {in: categories}},
-            ]
-        },
-        orderBy: { [sortField]: sortDirection },
-    });
+    try{
+        const reports = await prisma.report.findMany({
+            where: {
+                AND: [
+                    {status: {in: statuses}},
+                    {severity: {in: severities}},
+                    {category: {in: categories}},
+                ]
+            },
+            orderBy: { [sortField]: sortDirection },
+        });
 
-    return reports;
+        return reports; 
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 }
 
 export async function getUser({userId} : any){
-    const user = await prisma.user.findFirst({
-        where: { id: userId }
-    });
+    try{
+        const user = await prisma.user.findFirst({
+            where: { id: userId }
+        });
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 
     return user;
 }
@@ -240,35 +291,45 @@ export async function getUser({userId} : any){
 
 
 export async function getAllReports(){
-    const reports = await prisma.report.findMany();
-    return reports;
+    try{   
+        const reports = await prisma.report.findMany();
+        return reports;
+    }
+    catch (error){
+        console.error("error occured: " + error.message);
+    }
 }
 
 
 export async function deleteUser({user} : User){
 
-    if (user['role'] == "LANDLORD"){
-        // delete properties (-> delete property listings (-> delete associated property applications) )
-        await rawDeletePropertiesByLandlord({landlordId:user.id});        
+    try{   
+        if (user['role'] == "LANDLORD"){
+            // delete properties (-> delete property listings (-> delete associated property applications) )
+            await rawDeletePropertiesByLandlord({landlordId:user.id});        
+        }
+        else if (user['role'] == "CONSULTANT"){
+
+            // delete property applications made
+            await rawDeletePropertyApplicationsByUser({userId:user.id});
+        }
+
+        // for both roles, 'delete' reviews made about them and by them
+        await rawDeleteReviewByUser({userId:user.id});
+
+
+        // 'delete' from database by setting isDeleted to true
+        rawDeleteUser({userId:user.id});
+
+        const text : string = "Your account has been deleted";
+        sendEmail({user, text});
+
+        // confirm in logs
+        console.log("deleted user with id: " + user['id']);
     }
-    else if (user['role'] == "CONSULTANT"){
-
-        // delete property applications made
-        await rawDeletePropertyApplicationsByUser({userId:user.id});
+    catch (error){
+        console.error("error occured: " + error.message);
     }
-
-    // for both roles, 'delete' reviews made about them and by them
-    await rawDeleteReviewByUser({userId:user.id});
-
-
-    // 'delete' from database by setting isDeleted to true
-    rawDeleteUser({userId:user.id});
-
-    const text : string = "Your account has been deleted";
-    sendEmail({user, text});
-
-    // confirm in logs
-    console.log("deleted user with id: " + user['id']);
 
     return;
 }
