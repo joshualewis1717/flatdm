69a89ed99e25038166c8c30d1d7b65883ec2e695
Adam Siddiq <adamsiddiq.sidki@gmail.com>
2026-04-10 14:57:33 +0100

    removed full name and security thing from public view

--- src/components/shared/app-frame.tsx
+++ src/components/shared/app-frame.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { createContext, useContext } from "react";
 import Header from "@/components/shared/header";
-import Menu from "@/components/shared/Menu";
+import Menu from "@/components/shared/menu";
 import { Role } from "@prisma/client";
 
 type AppFrameProps = {
