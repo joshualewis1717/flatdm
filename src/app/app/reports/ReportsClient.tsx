a1d3650ccbc09a960d1c12a7ff96052fe6246ec3
AvniDhokia <avDhokia@protonmail.com>
2026-04-11 16:06:42 +0100

    restructured page

--- src/app/app/reports/ReportsClient.tsx
+++ src/app/app/reports/ReportsClient.tsx
@@ -114,171 +114,174 @@ export default function ReportsClient({ initialReports, users }: {initialReports
     <div className="p-4">
       <div className="mb-4 flex flex-col md:flex-row md:items-end md:justify-between gap-4">
 
-        
+        {/* filter and sorting panel */}
+        <div className="flex flex-row gap-10 rounded-xl border border-white/10 bg-[#1e1e1e] px-5 py-4 text-white">
 
-        {/* selection for statuses */}
-        <fieldset className="flex flex-col gap-2">
-          <legend className="font-semibold">Filter by status</legend>
+          {/* selection for statuses */}
+          <fieldset className="flex flex-col gap-2">
+            <legend className="font-semibold">Filter by status</legend>
 
-          {/* iterate through each option to make a checkbox and label */}
-          {(["OPEN", "UNDER_REVIEW", "RESOLVED"] as Status[]).map((status) => (
-            <label key={status} className="inline-flex items-center gap-2">
-              <Checkbox
-              checked={!!workingStatuses[status]}
-              onCheckedChange={() => toggleStatus(status)}
-              className="w-4 h-4"
-              />
-              <span>{status}</span>
-            </label>
-          ))}
-        </fieldset>
-
-        {/* selection for severities */}
-        <fieldset className="flex flex-col gap-2">
-          <legend className="font-semibold">Filter by severity</legend>
-
-          {/* iterate through all severities and make a checkbox and label */}
-          {(["UNRANKED", "LOW", "MEDIUM", "HIGH"] as Severity[]).map((severity) => (
-            <label key={severity} className="inline-flex items-center gap-2">
-              <Checkbox
-                checked={!!workingSeverities[severity]}
-                onCheckedChange={() => toggleSeverity(severity)}
+            {/* iterate through each option to make a checkbox and label */}
+            {(["OPEN", "UNDER_REVIEW", "RESOLVED"] as Status[]).map((status) => (
+              <label key={status} className="inline-flex items-center gap-2">
+                <Checkbox
+                checked={!!workingStatuses[status]}
+                onCheckedChange={() => toggleStatus(status)}
                 className="w-4 h-4"
-              />
-              <span>{severity}</span>
-            </label>
-          ))}
-        </fieldset>
+                />
+                <span>{status}</span>
+              </label>
+            ))}
+          </fieldset>
 
-        {/* selection for categories */}
-        <fieldset className="flex flex-col gap-2">
-          <legend className="font-semibold">Filter by category</legend>
+          {/* selection for severities */}
+          <fieldset className="flex flex-col gap-2">
+            <legend className="font-semibold">Filter by severity</legend>
 
-          {/* iterate through options and make checbox and label for each */}
-          {(["INAPPROPRIATE_CONTENT","FRAUD","HARASSMENT","FAKE_INFORMATION","IMPERSONATION","OTHER"] as Category[]
-          ).map((category) => (
-            <label key={category} className="inline-flex items-center gap-2">
-              <Checkbox
-                checked={!!workingCategories[category]}
-                onCheckedChange={() => toggleCategory(category)}
-                className="w-4 h-4"
-              />
-              <span>{category}</span>
-            </label>
-          ))}
-        </fieldset>
+            {/* iterate through all severities and make a checkbox and label */}
+            {(["UNRANKED", "LOW", "MEDIUM", "HIGH"] as Severity[]).map((severity) => (
+              <label key={severity} className="inline-flex items-center gap-2">
+                <Checkbox
+                  checked={!!workingSeverities[severity]}
+                  onCheckedChange={() => toggleSeverity(severity)}
+                  className="w-4 h-4"
+                />
+                <span>{severity}</span>
+              </label>
+            ))}
+          </fieldset>
 
-        {/* sorting, search and reset buttons */}
-        <div className="flex items-center gap-4">
-          
-          {/* drop down menu for choosing option to sort by (modified at or created at) */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <button className="border rounded px-2 py-1 flex items-center gap-2">
-                <span className="text-sm font-medium">Sort by</span>
-                <span className="text-sm">
-                  {workingSortField === "modifiedAt" ? "Modified At" : "Created At"}
-                </span>
-              </button>
-            </DropdownMenuTrigger>
+          {/* selection for categories */}
+          <fieldset className="flex flex-col gap-2">
+            <legend className="font-semibold">Filter by category</legend>
 
-            <DropdownMenuContent align="start" sideOffset={8}>
-              <DropdownMenuGroup>
-                <DropdownMenuRadioGroup
-                  value={workingSortField}
-                  onValueChange={(val: "modifiedAt" | "createdAt") =>
-                    setWorkingSortField(val)
-                  }
+            {/* iterate through options and make checbox and label for each */}
+            {(["INAPPROPRIATE_CONTENT","FRAUD","HARASSMENT","FAKE_INFORMATION","IMPERSONATION","OTHER"] as Category[]
+            ).map((category) => (
+              <label key={category} className="inline-flex items-center gap-2">
+                <Checkbox
+                  checked={!!workingCategories[category]}
+                  onCheckedChange={() => toggleCategory(category)}
+                  className="w-4 h-4"
+                />
+                <span>{category}</span>
+              </label>
+            ))}
+          </fieldset>
+
+          {/* sorting, search and reset buttons */}
+          <div className="flex items-center gap-4">
+            
+              {/* drop down menu for choosing option to sort by (modified at or created at) */}
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <button className="border rounded px-2 py-1 flex items-center gap-2">
+                    <span className="text-sm font-medium">Sort by</span>
+                    <span className="text-sm">
+                      {workingSortField === "modifiedAt" ? "Modified At" : "Created At"}
+                    </span>
+                  </button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent align="start" sideOffset={8}>
+                  <DropdownMenuGroup>
+                    <DropdownMenuRadioGroup
+                      value={workingSortField}
+                      onValueChange={(val: "modifiedAt" | "createdAt") =>
+                        setWorkingSortField(val)
+                      }
+                    >
+                      <DropdownMenuRadioItem value="modifiedAt">
+                        Modified At
+                      </DropdownMenuRadioItem>
+                      <DropdownMenuRadioItem value="createdAt">
+                        Created At
+                      </DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+
+              {/* drop down menu for order of sorting (desc or asc) */}
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <button className="border rounded px-2 py-1 flex items-center gap-2">
+                    <span className="text-sm font-medium">Direction</span>
+                    <span className="text-sm">
+                      {workingSortDirection === "desc" ? "Newest first" : "Oldest first"}
+                    </span>
+                  </button>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent align="start" sideOffset={8}>
+                  <DropdownMenuGroup>
+                    <DropdownMenuRadioGroup
+                      value={workingSortDirection}
+                      onValueChange={(val: "asc" | "desc") =>
+                        setWorkingSortDirection(val)
+                      }
+                    >
+                      <DropdownMenuRadioItem value="desc">
+                        Newest first
+                      </DropdownMenuRadioItem>
+                      <DropdownMenuRadioItem value="asc">
+                        Oldest first
+                      </DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              {/* search and reset buttons */}
+              <div className="flex gap-2">
+
+                {/* "search" button that actually just sorts and filters the reports */}
+                <button
+                  onClick={handleApplyFilters}
+                  className="bg-blue-600 text-white px-3 py-1 rounded"
                 >
-                  <DropdownMenuRadioItem value="modifiedAt">
-                    Modified At
-                  </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="createdAt">
-                    Created At
-                  </DropdownMenuRadioItem>
-                </DropdownMenuRadioGroup>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+                  Search
+                </button>
 
+                {/* reset to original settings where all options wanted */}
+                <button
+                  onClick={() => {
+                    setWorkingStatuses({
+                      "OPEN": true,
+                      "UNDER_REVIEW": true,
+                      "RESOLVED": true,
+                    });
 
-          {/* drop down menu for order of sorting (desc or asc) */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <button className="border rounded px-2 py-1 flex items-center gap-2">
-                <span className="text-sm font-medium">Direction</span>
-                <span className="text-sm">
-                  {workingSortDirection === "desc" ? "Newest first" : "Oldest first"}
-                </span>
-              </button>
-            </DropdownMenuTrigger>
+                    setWorkingSeverities({
+                      "UNRANKED": true,
+                      "LOW": true,
+                      "MEDIUM": true,
+                      "HIGH": true,
+                    });
 
-            <DropdownMenuContent align="start" sideOffset={8}>
-              <DropdownMenuGroup>
-                <DropdownMenuRadioGroup
-                  value={workingSortDirection}
-                  onValueChange={(val: "asc" | "desc") =>
-                    setWorkingSortDirection(val)
-                  }
+                    setWorkingCategories({
+                      "INAPPROPRIATE_CONTENT": true,
+                      "FRAUD": true,
+                      "HARASSMENT": true,
+                      "FAKE_INFORMATION": true,
+                      "IMPERSONATION": true,
+                      "OTHER": true,
+                    });
+                    setWorkingSortField("modifiedAt");
+                    setWorkingSortDirection("desc");
+                    handleApplyFilters();
+                  }}
+                  className="border px-3 py-1 rounded"
                 >
-                  <DropdownMenuRadioItem value="desc">
-                    Newest first
-                  </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="asc">
-                    Oldest first
-                  </DropdownMenuRadioItem>
-                </DropdownMenuRadioGroup>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+                  Reset
+                </button>
+              </div>
+          </div>
 
-          {/* search and reset buttons */}
-          <div className="flex gap-2">
-
-            {/* "search" button that actually just sorts and filters the reports */}
-            <button
-              onClick={handleApplyFilters}
-              className="bg-blue-600 text-white px-3 py-1 rounded"
-            >
-              Search
-            </button>
-
-            {/* reset to original settings where all options wanted */}
-            <button
-              onClick={() => {
-                setWorkingStatuses({
-                  "OPEN": true,
-                  "UNDER_REVIEW": true,
-                  "RESOLVED": true,
-                });
-
-                setWorkingSeverities({
-                  "UNRANKED": true,
-                  "LOW": true,
-                  "MEDIUM": true,
-                  "HIGH": true,
-                });
-
-                setWorkingCategories({
-                  "INAPPROPRIATE_CONTENT": true,
-                  "FRAUD": true,
-                  "HARASSMENT": true,
-                  "FAKE_INFORMATION": true,
-                  "IMPERSONATION": true,
-                  "OTHER": true,
-                });
-                setWorkingSortField("modifiedAt");
-                setWorkingSortDirection("desc");
-                handleApplyFilters();
-              }}
-              className="border px-3 py-1 rounded"
-            >
-              Reset
-            </button>
           </div>
         </div>
 
-      </div>
 
       <PaginationBar currentPage={page} totalPages={totalPages} onPageChange={gotoPage} />
 
--- src/app/app/reports/readMe.md
+++ src/app/app/reports/readMe.md
@@ -1,6 +1,5 @@
 # moderator requirements
 
 - OTHER
-    - [ ] use davud's pagination
     - [ ] use inputField by fahi
     - [ ] make filter bit nicer
